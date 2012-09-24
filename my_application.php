96f98c66afd05f6a60f6360cca7f0a7e37b14af8
Ostrovskiy Grigoriy <greevex@gmail.com>
2012-09-24 18:33:44 +0400

    remove my application

--- my_application.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/init.phar';
-
-$ssh = new \mpr\net\ssh("bots02.sdstream.ru", 22, 'ostrovskiy', '123456Fe', '/home/ostrovskiy/.ssh/id_rsa.pub', '/home/ostrovskiy/.ssh/id_rsa');
-$ssh->connect();
-var_dump($ssh->execute("ls -la /", true));
