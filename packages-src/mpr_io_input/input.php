a35a54a1359105a31a2e05a6674007cafb7279f6
Ostrovskiy Grigoriy <greevex@gmail.com>
2012-10-22 19:15:07 +0400

    Add phpDoc blocks
    Add phpDoc blocks to mpr_io_input

--- packages-src/mpr_io_input/input.php
+++ packages-src/mpr_io_input/input.php
@@ -2,23 +2,62 @@
 namespace mpr\io;
 
 /**
- * Input
+ * Input package
+ *
+ * Parses all availible input data from command-line interface or from web-server
  *
  * @author GreeveX <greevex@gmail.com>
  */
 class input
 {
 
+    /**
+     * Look-up in $_GET super-global var
+     */
     const SC_GET = '_GET';
+
+    /**
+     * Look-up in $_POST super-global var
+     */
     const SC_POST = '_POST';
+
+    /**
+     * Look-up in $_REQUEST super-global var
+     */
     const SC_REQUEST = '_REQUEST';
+
+    /**
+     * Look-up in $_FILES super-global var
+     */
     const SC_FILES = '_FILES';
+
+    /**
+     * Look-up in command-line arguments
+     */
     const SC_ARGV = 'argv';
+
+    /**
+     * Look-up in internal parsed data
+     */
     const SC_INTERNAL = 'self::$INTERNAL';
+
+    /**
+     * Param isn't exists
+     */
     const NOT_EXISTS = 'param_not_exists';
 
+    /**
+     * Internal parsed data
+     *
+     * @var array
+     */
     private static $INTERNAL = array();
 
+    /**
+     * Construct new input object
+     *
+     * If global argv var exists we'll parse it
+     */
     public function __construct()
     {
         if(isset($GLOBALS['argv'])) {
@@ -66,6 +105,13 @@ class input
         return $out;
     }
 
+    /**
+     * Read line from user by command-line request
+     *
+     * @param string $requestMessage Request message
+     * @param string $defaultValue Default value will be shown to user
+     * @return string User-typed data or default value
+     */
     public function readLine($requestMessage = "", $defaultValue = "")
     {
         if(!empty($defaultValue)) {
@@ -78,6 +124,12 @@ class input
         return $input;
     }
 
+    /**
+     * Import arguments from super-global var
+     *
+     * @param string $request_type Select it by constants self::SC_*
+     * @param bool $clean Clear already parsed data
+     */
     public function importArguments($request_type = self::SC_REQUEST, $clean = true)
     {
         switch($request_type)
@@ -92,6 +144,7 @@ class input
                 $data = $_POST;
                 break;
             case self::SC_REQUEST:
+            default:
                 $data = $_REQUEST;
                 break;
         }
@@ -102,11 +155,22 @@ class input
         }
     }
 
+    /**
+     * Export parsed data as array
+     *
+     * @return array
+     */
     public function export()
     {
         return self::$INTERNAL;
     }
 
+    /**
+     * Set arguments to internal parsed data
+     *
+     * @param array $array
+     * @param bool $clean Clear already exists data
+     */
     public function setArguments($array, $clean = true)
     {
         if($clean) {
@@ -116,6 +180,13 @@ class input
         }
     }
 
+    /**
+     * Get param by name and request type
+     *
+     * @param $name
+     * @param $request_type
+     * @return mixed|string
+     */
     private function getParam($name, $request_type)
     {
         $req = $request_type == self::SC_INTERNAL ? self::$INTERNAL : ${$request_type};
@@ -123,6 +194,13 @@ class input
                         $req[$name] : self::NOT_EXISTS;
     }
 
+    /**
+     * Get string from request
+     *
+     * @param $name
+     * @param string $request_type
+     * @return null|string Null if param not exists
+     */
     public function getString($name, $request_type = self::SC_INTERNAL)
     {
         $param = $this->getParam($name, $request_type);
@@ -132,6 +210,13 @@ class input
         return strval($param);
     }
 
+    /**
+     * Get boolean from request
+     *
+     * @param $name
+     * @param string $request_type
+     * @return bool|null
+     */
     public function getBoolean($name, $request_type = self::SC_INTERNAL)
     {
         $param = $this->getParam($name, $request_type);
@@ -141,6 +226,13 @@ class input
         return (bool)($param == true || $param == 'true');
     }
 
+    /**
+     * Get integer from request
+     *
+     * @param $name
+     * @param string $request_type
+     * @return int|null Null if param not exists
+     */
     public function getInteger($name, $request_type = self::SC_INTERNAL)
     {
         $param = $this->getParam($name, $request_type);
@@ -150,6 +242,13 @@ class input
         return intval($param);
     }
 
+    /**
+     * Get float from request
+     *
+     * @param $name
+     * @param string $request_type
+     * @return float|null Null if param not exists
+     */
     public function getFloat($name, $request_type = self::SC_INTERNAL)
     {
         $param = $this->getParam($name, $request_type);
@@ -159,6 +258,13 @@ class input
         return floatval($param);
     }
 
+    /**
+     * Get array from request
+     *
+     * @param $name
+     * @param string $request_type
+     * @return array|null Null if param not exists
+     */
     public function getArray($name, $request_type = self::SC_INTERNAL)
     {
         $param = $this->getParam($name, $request_type);
