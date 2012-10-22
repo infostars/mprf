cd30913f50bbfdefc3ee86ee4cc249be3b981041
Ostrovskiy Grigoriy <greevex@gmail.com>
2012-10-22 18:38:24 +0400

    Add phpDoc blocks
    Add phpDoc blocks into mpr_cache, mpr_cache_file packages

--- packages-src/mpr_cache/cache.php
+++ packages-src/mpr_cache/cache.php
@@ -5,18 +5,35 @@ use \mpr\debug\log;
 use \mpr\config;
 use \mpr\interfaces;
 
+/**
+ * Cache builder
+ *
+ * Constructs using driver package name
+ *
+ * @author Ostrovskiy Grigoriy <greevex@gmail.com>
+ */
 class cache
 implements interfaces\cache
 {
 
+    /**
+     * Cache driver instances
+     *
+     * @var array
+     */
     protected static $instances = array();
 
     /**
-     * @var cache_interface
+     * Cache driver backend
+     *
+     * @see interfaces\cache
+     * @var cache
      */
     private $backend;
 
     /**
+     * Factory cache by driver package name
+     *
      * @static
      * @param null $driver_packageName
      * @see \mpr\interfaces\cache
@@ -34,24 +51,49 @@ implements interfaces\cache
         return self::$instances[$driver_packageName];
     }
 
+    /**
+     * Build cache by driver's package name
+     *
+     * @param $driver_packageName
+     */
     public function __construct($driver_packageName)
     {
         $driver = config::getClassName($driver_packageName);
         $this->backend = new $driver();
     }
 
+    /**
+     * Set value by key
+     *
+     * @param $key
+     * @param $value
+     * @param int $expire
+     * @return mixed
+     */
     public function set($key, $value, $expire = 3600)
     {
         log::put("Set {$key}", config::getPackageName(__CLASS__));
         return $this->backend->set($key, $value, $expire);
     }
 
+    /**
+     * Get value by key
+     *
+     * @param $key
+     * @return mixed
+     */
     public function get($key)
     {
         log::put("Get {$key}", config::getPackageName(__CLASS__));
         return $this->backend->get($key);
     }
 
+    /**
+     * Check is key exists
+     *
+     * @param $key
+     * @return mixed
+     */
     public function exists($key)
     {
         $exists = $this->backend->exists($key);
@@ -59,30 +101,56 @@ implements interfaces\cache
         return $exists;
     }
 
+    /**
+     * Remove record from cache by key
+     *
+     * @param $key
+     * @return mixed
+     */
     public function remove($key)
     {
         log::put("Remove {$key}", config::getPackageName(__CLASS__));
         return $this->backend->remove($key);
     }
 
+    /**
+     * WARNING! Clear all cache
+     *
+     * @return mixed
+     */
     public function clear()
     {
         log::put("WARNING! CLEARING ALL CACHE!", config::getPackageName(__CLASS__));
         return $this->backend->clear();
     }
 
+    /**
+     * Enable auto commit changes
+     *
+     * @return mixed
+     */
     public function enableAutoCommit()
     {
         log::put("Transaction - autoCommit enabled", config::getPackageName(__CLASS__));
         return $this->backend->enableAutoCommit();
     }
 
+    /**
+     * Disable auto commit changes
+     *
+     * @return mixed
+     */
     public function disableAutoCommit()
     {
         log::put("Transaction - autoCommit disabled", config::getPackageName(__CLASS__));
         return $this->backend->disableAutoCommit();
     }
 
+    /**
+     * Commit changes
+     *
+     * @return mixed
+     */
     public function commit()
     {
         log::put("Transaction - Commiting...", config::getPackageName(__CLASS__));
--- packages-src/mpr_cache_file/file.php
+++ packages-src/mpr_cache_file/file.php
@@ -15,10 +15,25 @@ extends cache
 implements cache_interface
 {
 
+    /**
+     * Cache in-memory data
+     *
+     * @var array
+     */
     private $data = array();
 
+    /**
+     * Auto commit flag
+     *
+     * @var bool
+     */
     private $autoCommit = true;
 
+    /**
+     * Build cache filename
+     *
+     * @return string
+     */
     private function getCacheFilename()
     {
         static $cache_filename;
@@ -29,51 +44,105 @@ implements cache_interface
         return $cache_filename;
     }
 
+    /**
+     * Commit changes
+     *
+     * @return bool
+     */
     public function commit()
     {
         file_put_contents($this->getCacheFilename(), json_encode($this->data));
+        return true;
     }
 
+    /**
+     * Initialize driver and load data
+     */
     public function __construct()
     {
         $this->loadData();
     }
 
+    /**
+     * Load data from file to memory
+     *
+     * @return bool
+     */
     protected function loadData()
     {
         if(file_exists($this->getCacheFilename())) {
             $this->data = json_decode(file_get_contents($this->getCacheFilename()), true);
         }
+        return true;
     }
 
+    /**
+     * Enable auto commit changes
+     *
+     * @return bool
+     */
     public function enableAutoCommit()
     {
         $this->autoCommit = true;
+        return true;
     }
 
+    /**
+     * Disable auto commit changes
+     *
+     * @return bool
+     */
     public function disableAutoCommit()
     {
         $this->autoCommit = false;
+        return true;
     }
 
+    /**
+     * Set value by key
+     *
+     * @param string $key
+     * @param mixed $value
+     * @param int $expire
+     * @return bool
+     */
     public function set($key, $value, $expire = 0)
     {
         $this->data[$key] = $value;
         if($this->autoCommit) {
             $this->commit();
         }
+        return true;
     }
 
+    /**
+     * Get value by key
+     *
+     * @param string $key
+     * @return mixed|null
+     */
     public function get($key)
     {
         return isset($this->data[$key]) ? $this->data[$key] : null;
     }
 
+    /**
+     * Check is key exists
+     *
+     * @param string $key
+     * @return bool Result
+     */
     public function exists($key)
     {
         return isset($this->data[$key]);
     }
 
+    /**
+     * Remove record from cache by key
+     *
+     * @param string $key
+     * @return bool Result
+     */
     public function remove($key)
     {
         if(isset($this->data[$key])) {
@@ -82,11 +151,18 @@ implements cache_interface
         if($this->autoCommit) {
             $this->commit();
         }
+        return true;
     }
 
+    /**
+     * WARNING! Clear all cache
+     *
+     * @return bool
+     */
     public function clear()
     {
         $this->data = array();
         $this->commit();
+        return true;
     }
 }
