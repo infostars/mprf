260c9a7fe7fefcf5197711330b4dfcf1ba43a770
Demin Petr <demin@infostars.ru>
2012-11-07 12:02:35 +0400

    remove tearDown function

--- packages-src/mpr_net_httprequest/tests/httpRequestTest.php
+++ packages-src/mpr_net_httprequest/tests/httpRequestTest.php
@@ -35,14 +35,6 @@ class httpRequestTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
-
-    /**
      * @covers mpr\net\httpRequest::prepare
      */
     public function testPrepare()
