eb3796dd5fdfd17ae68daebb3c50b2fcc6af7eec
Ostrovskiy Grigoriy <greevex@gmail.com>
2012-11-16 17:59:34 +0400

    init update

--- index/init.php
+++ index/init.php
@@ -24,39 +24,66 @@ if(!defined('APP_ENV_PROD')) {
  * dev = src
  */
 spl_autoload_register(function ($package) {
+    static $loading_cache;
+    static $cache;
+
+    // env
     $env = APP_ENV_PROD === true ? 'prod' : 'dev';
+
+    // normalize package name
     if(strpos($package, '\\') === 0) {
         $package = mb_substr($package, 1);
     }
     $package = str_replace("\\", "_", $package);
-    switch(strtolower($env)) {
-        case "prod":
-            $packageArray = explode("_", $package);
-            array_pop($packageArray);
-            $packagePath = APP_ROOT . "/" . implode("/", $packageArray) . "/{$package}.phar";
-            break;
-        case "dev":
-        default:
-            $packagePath = APP_ROOT;
-            if(!isset($GLOBALS['dev_packages_path']) || !is_array($GLOBALS['dev_packages_path'])) {
-                print "Error loading `dev_packages_path` from init.config.php!\n";
-            }
-            foreach($GLOBALS['dev_packages_path'] as $dev_packages_path) {
-                $packagePath = $dev_packages_path . "/" . strtolower($package);
-                $manifest_file = "{$packagePath}/manifest.mpr.json";
-                if(file_exists($manifest_file)) {
-                    $initFile = json_decode(file_get_contents($manifest_file), 1)['package']['init'];
-                    $packagePath .= "/{$initFile}";
-                    break;
+
+    $server_uniq = crc32(__DIR__);
+
+    // build cache key
+    $cache_key = "init:{$env}:{$package}:{$server_uniq}";
+
+    // load cache
+    if(!$loading_cache && $cache == null) {
+        $loading_cache = 1;
+        $cache = cache::factory();
+        $loading_cache = 0;
+    }
+
+    // get path from cache or search it
+    if(!$loading_cache && isset($cache) && $cache->exists($cache_key)) {
+        $packagePath = $cache->get($cache_key);
+    } else {
+        switch(strtolower($env)) {
+            case "prod":
+                $packageArray = explode("_", $package);
+                array_pop($packageArray);
+                $packagePath = APP_ROOT . "/" . implode("/", $packageArray) . "/{$package}.phar";
+                break;
+            case "dev":
+            default:
+                error_reporting(E_ALL);
+                ini_set('display_errors', 1);
+                $packagePath = APP_ROOT;
+                if(!isset($GLOBALS['dev_packages_path']) || !is_array($GLOBALS['dev_packages_path'])) {
+                    print "Error loading `dev_packages_path` from init.config.php!\n";
                 }
-            }
+                foreach($GLOBALS['dev_packages_path'] as $dev_packages_path) {
+                    $packagePath = $dev_packages_path . "/" . strtolower($package);
+                    $manifest_file = "{$packagePath}/manifest.mpr.json";
+                    if(file_exists($manifest_file)) {
+                        $initFile = json_decode(file_get_contents($manifest_file), 1)['package']['init'];
+                        $packagePath .= "/{$initFile}";
+                        break;
+                    }
+                }
+        }
+        if(isset($cache)) {
+            $cache->set($cache_key, $packagePath, 3600);
+        }
     }
     if(file_exists($packagePath)) {
         require_once $packagePath;
-        \mpr\debug\log::put("Loaded {$package} ({$packagePath})", "init");
+        \mpr\debug\log::put("Loaded {$package}", "init");
     } else {
-        \mpr\debug\log::put("Skipped {$package} ({$packagePath})", "init");
+        \mpr\debug\log::put("Skipped {$package}", "init");
     }
 });
-
-__halt_compiler();
