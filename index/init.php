679bdea20e9a3aa9dda2febca7c1091b5405737c
Ostrovskiy Grigoriy <greevex@gmail.com>
2012-11-16 20:41:42 +0400

    update init

--- index/init.php
+++ index/init.php
@@ -3,46 +3,46 @@ namespace mpr;
 
 $init_config_file = str_replace("phar://", '', dirname(__DIR__) . '/config.php');
 
-if(!file_exists($init_config_file)) {
+if (!file_exists($init_config_file)) {
     print "Unable to load file {$init_config_file}!\n";
     exit(1);
 }
 require_once $init_config_file;
-if(!defined('APP_ROOT')) {
+if (!defined('APP_ROOT')) {
     print "Error loading `APP_ROOT` from {$init_config_file}!\n";
     exit(1);
 }
-if(!isset($GLOBALS['APP_ENV'])) {
+if (!isset($GLOBALS['APP_ENV'])) {
     print "Error loading `APP_ENV` from {$init_config_file}!\n";
     exit(1);
 }
-if(!isset($GLOBALS['PACKAGES_TYPE'])) {
+if (!isset($GLOBALS['PACKAGES_TYPE'])) {
     print "Error loading `PACKAGES_TYPE` from {$init_config_file}!\n";
     exit(1);
 }
-if(!isset($GLOBALS['PACKAGES_CACHE_EXPIRE'])) {
+if (!isset($GLOBALS['PACKAGES_CACHE_EXPIRE'])) {
     print "Error loading `PACKAGES_CACHE_EXPIRE` from {$init_config_file}!\n";
     exit(1);
 }
-if(!isset($GLOBALS['PACKAGES_PATH']) || !is_array($GLOBALS['PACKAGES_PATH'])) {
+if (!isset($GLOBALS['PACKAGES_PATH']) || !is_array($GLOBALS['PACKAGES_PATH'])) {
     print "Error loading `PACKAGES_PATH` from {$init_config_file}!\n";
     exit(1);
 }
 
-spl_autoload_register(function ($package_name) use ($init_config_file) {
+spl_autoload_register(function ($package_name) {
 
     // normalize package name
-    if(strpos($package_name, '\\') === 0) {
+    if (strpos($package_name, '\\') === 0) {
         $package_name = mb_substr($package_name, 1);
     }
     $package_name = str_replace("\\", "_", $package_name);
 
     static $server_uniq;
-    if(!isset($server_uniq)) {
+    if (!isset($server_uniq)) {
         $server_uniq = crc32(__DIR__);
     }
 
-    switch(strtolower($GLOBALS['PACKAGES_TYPE'])) {
+    switch (strtolower($GLOBALS['PACKAGES_TYPE'])) {
         case "phar":
             $packageArray = explode("_", $package_name);
             array_pop($packageArray);
@@ -51,17 +51,17 @@ spl_autoload_register(function ($package_name) use ($init_config_file) {
         case "src":
         default:
             $packagePath = APP_ROOT;
-            foreach($GLOBALS['PACKAGES_PATH'] as $dev_packages_path) {
+            foreach ($GLOBALS['PACKAGES_PATH'] as $dev_packages_path) {
                 $packagePath = $dev_packages_path . "/" . strtolower($package_name);
                 $manifest_file = "{$packagePath}/manifest.mpr.json";
-                if(file_exists($manifest_file)) {
+                if (file_exists($manifest_file)) {
                     $initFile = json_decode(file_get_contents($manifest_file), 1)['package']['init'];
                     $packagePath .= "/{$initFile}";
                     break;
                 }
             }
     }
-    if(file_exists($packagePath)) {
+    if (file_exists($packagePath)) {
         require_once $packagePath;
         \mpr\debug\log::put("Loaded {$package_name}", "init");
     } else {
