d343faecc9830bb8124385795377e7304601f027
Gregory Ostrovskiy <greevex@gmail.com>
2013-04-22 18:00:12 +0400

    smarty ICONVert modifier added

--- packages-src/mpr_view_smarty/Smarty/libs/plugins/modifier.iconvert.php
+++ packages-src/mpr_view_smarty/Smarty/libs/plugins/modifier.iconvert.php
@@ -2,14 +2,14 @@
 /*
  * Smarty plugin
  * -------------------------------------------------------------
- * Файл:         modifier.convert.php
+ * Файл:         modifier.iconvert.php
  * Тип:          modifier
- * Имя:          convert
+ * Имя:          iconvert
  * Назначение:   Конвертация из одной кодировки в другую функцией iconv или mb_convert_encoding.
  * Использование в шаблоне:   {$templ_var|convert:'UTF-8':'CP1251':false}
  * -------------------------------------------------------------
  */
-function smarty_modifier_convert( $string, $from = 'CP1251', $to = 'UTF-8', $mb = false )
+function smarty_modifier_iconvert( $string, $from = 'CP1251', $to = 'UTF-8', $mb = false )
 {
     if (!$mb){
         $conv_string = iconv($from, $to, $string);
