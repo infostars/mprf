595896a4bdd6a1f44954f0a8480711ec2e93ac05
Gregory Ostrovskiy <greevex@gmail.com>
2013-05-22 19:09:22 +0400

    ipselect update

--- packages-src/mpr_helper_ipselect/ipselect.php
+++ packages-src/mpr_helper_ipselect/ipselect.php
@@ -12,7 +12,9 @@ class ipselect
     /**
      * @var string $ip_select_command - shell command
      */
-    private static $ip_select_command = 'ifconfig | grep venet0:';
+    private static $ip_select_command = <<<CMD
+/sbin/ip a | grep "inet " | awk '{print $2}' | cut -d "/" -f 1 | grep -v '127.0.0'
+CMD;
 
     /**
      * Return ip array on local machine
@@ -24,10 +26,10 @@ class ipselect
         static $network_interfaces_list;
 
         if($network_interfaces_list == null) {
-            $exec = self::$ip_select_command . " | awk '{print $1}'";
+            $exec = self::$ip_select_command;
             $network_interfaces_string = trim(shell_exec($exec));
             if(strlen($network_interfaces_string) > 0) {
-                $network_interfaces_list = explode("\n", $network_interfaces_string);;
+                $network_interfaces_list = explode("\n", $network_interfaces_string);
             } else {
                 $network_interfaces_list = [];
             }
